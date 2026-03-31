ae0fdf2a2c3a716dcafabbaa100752cb4e2352aa
Colin Kim <121141519+colinvkim@users.noreply.github.com>
2026-03-31 00:57:13 -0700

    feat: redesign UI to make site faster

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,17 @@
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
   site: "https://best-builds.colinkim.dev",
-
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: "Inter",
-      cssVariable: "--font-inter",
-    },
-  ],
 
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react()],
+  adapter: vercel(),
 });
